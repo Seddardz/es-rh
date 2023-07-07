e22405b65de071961d570fd24c2466dd31c2f0d4
seddar <ephmaghni@gmail.com>
2023-07-08 01:38:31 +0200

    Remplir table user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use Hash;
+use App\Models\User;
 class AuthController extends Controller
 {
     public function index(Request $request)
@@ -18,6 +19,16 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         return view('register');
+    }
+
+        public function register_post(Request $request)
+    {
+        $user = new User;
+        $user->name = trim($request->name);
+        $user->email = trim($request->email);
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect('/')->with('success','Register Successfully');
     }
 }
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -24,9 +24,10 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Register</p>
 
-      <form action="" method="post">
+      <form action="{{ url('register_post') }}" method="post">
+    {{ csrf_field() }}
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Name">
+          <input type="text" class="form-control" placeholder="Name" name="name">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -35,7 +36,7 @@
         </div>
 
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" class="form-control" placeholder="Email" name="email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -44,13 +45,23 @@
         </div>
 
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Password" name="password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+
+                <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
